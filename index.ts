ea1415897f8701aab85cf173d85b08acb21fc042
indexestech <indexestech@outlook.com>
2023-12-28 12:40:36 +0800

    create CommitTx

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
 import mempoolJS from "@mempool/mempool.js";
-import bitcoin, { Network, Payment, script as bscript, initEccLib } from "bitcoinjs-lib";
+import bitcoin, { Network, Payment, Psbt, script as bscript, initEccLib } from "bitcoinjs-lib";
 import { toXOnly, tweakInternalPubKey } from "bitcoinjs-lib/src/psbt/bip371";
 
 import * as ecc from "tiny-secp256k1";
 initEccLib(ecc);
 
-import ECPairFactory, { ECPairInterface } from "ecpair";
+import ECPairFactory, { ECPairAPI, ECPairInterface } from "ecpair";
 import { MempoolReturn } from "@mempool/mempool.js/lib/interfaces/index";
+import { AddressTxsUtxo } from "@mempool/mempool.js/lib/interfaces/bitcoin/addresses";
 const ECPair = ECPairFactory(ecc);
+
+const validator = (pubkey: Buffer, msghash: Buffer, signature: Buffer): boolean =>
+  ECPair.fromPublicKey(pubkey).verify(msghash, signature);
+
+const schnorrValidator = (pubkey: Buffer, msghash: Buffer, signature: Buffer): boolean =>
+  ecc.verifySchnorr(msghash, pubkey, signature);
 
 type InscriptionRequest = {
   receiverAddr: string;
@@ -36,6 +43,31 @@ async function inscribe(
   let total = 0;
   for (let i = 0; i < inscriptionTxs.length; i++) {
     total += inscriptionTxs[i].value;
+  }
+
+  const utxos = await client.bitcoin.addresses.getAddressTxsUtxo({ address: payAddr });
+  let balance = 0;
+
+  for (let i = 0; i < utxos.length; i++) {
+    balance += utxos[i].value;
+  }
+
+  if (total >= balance) {
+    throw new Error("Insufficient Balance");
+  }
+
+  const signer = ECPair.fromWIF(process.env.PAY_PK_WIF!, network);
+
+  let commitTx = await createCommitTx(network, payAddr, signer, commitFee, utxos, inscriptionTxs);
+
+  // const estimatedVSize = tx.virtualSize();
+
+  console.log("hex", commitTx.toHex());
+  try {
+    const txId = await client.bitcoin.transactions.postTx({ txhex: commitTx.toHex() });
+    console.log("tx:", txId);
+  } catch (err) {
+    console.error("err", err);
   }
 }
 type InscriptionTx = {
@@ -116,16 +148,74 @@ function estimateRevealTxFee(
   }
 }
 
-async function findUtxo(client: MempoolReturn, address: string, target: number) {
-  const utxos = await client.bitcoin.addresses.getAddressTxsUtxo({ address });
+async function createCommitTx(
+  network: bitcoin.networks.Network,
+  payAddr: string,
+  signer: ECPairInterface,
+  feeRate: number,
+  utxos: AddressTxsUtxo[],
+  inscriptionTxs: InscriptionTx[]
+) {
+  const script = bitcoin.address.toOutputScript(payAddr, network);
+  const psbt = new Psbt({ network });
+  //psbt.setMaximumFeeRate(500);
+  let total = 0;
+  for (let i = 0; i < utxos.length; i++) {
+    const unspend = utxos[i];
+    psbt.addInput({
+      hash: unspend.txid,
+      index: unspend.vout,
+      witnessUtxo: { script, value: unspend.value },
+    });
+    total += unspend.value;
+  }
+
+  let spend = 0;
+  for (let i = 0; i < inscriptionTxs.length; i++) {
+    const ins = inscriptionTxs[i];
+    psbt.addOutput({ address: ins.payment.address!, value: ins.value });
+    spend += ins.value;
+  }
+  psbt.addOutput({ address: payAddr, value: 0 });
+
+  const estimatedSize = psbt.data.inputs.reduce(
+    (acc, input) => {
+      return acc + input.witnessUtxo!.script.length + 41; // 41 is the approximate size of other input fields
+    },
+    psbt.txOutputs.reduce((acc, output) => acc + 8 + output.script.length, 10)
+  );
+
+  const estimatedFee = Math.ceil(estimatedSize * feeRate);
+
+  const chargeIndex = psbt.txOutputs.length - 1;
+
+  psbt.txOutputs[chargeIndex].value += total - spend - estimatedFee;
+  console.log(
+    "total",
+    total,
+    "spend",
+    spend,
+    "estimatedFee",
+    estimatedFee,
+    "return",
+    psbt.txOutputs[chargeIndex].value
+  );
+  if (psbt.txOutputs[chargeIndex].value <= 0) {
+    throw new Error("INVALID_BALANCE_TO_GAS");
+  }
+
+  psbt.signAllInputs(signer);
+  psbt.finalizeAllInputs();
+  const vFeeRate = psbt.getFeeRate();
+  const vFee = psbt.getFee();
+  const tx = psbt.extractTransaction();
+  const vSize = tx.virtualSize();
+
+  console.log("estSize:", estimatedSize, "vsize:", vSize, vFeeRate, vFee);
+  return tx;
 }
 
-await inscribe(true, "tb1qp6zk2htw5scqpw7mf3nmhhkmrqdtm0z6edt5py", 200, 200, true, [
-  {
-    receiverAddr: "tb1qcvts8jpr437382gp74vrsaa9du6tvceaz74p7j",
-    content: Buffer.from("hello world", "utf8"),
-    contentType: "text/plain",
-  },
+await inscribe(true, "tb1qp6zk2htw5scqpw7mf3nmhhkmrqdtm0z6edt5py", 50, 200, true, [
   {
     receiverAddr: "tb1qcvts8jpr437382gp74vrsaa9du6tvceaz74p7j",
     content: Buffer.from("hello world", "utf8"),
